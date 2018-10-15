02deca39cf658b95d284d843da82b085007ef02e
Dan Bath <danbath@gmail.com>
2018-10-15 09:47:37 +0200

    added distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='videoStitch',
+      version='0.1.0',
+      description='tools for stitching videos',
+      author='Dan Bath',
+      author_email='danbath@gmail.com',
+      url='https://www.danbath.ca',
+      py_modules=['undistort','utilities','calibrate'],
+     )
